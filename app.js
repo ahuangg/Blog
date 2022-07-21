bf104350a457836c25e76320afb4d4e5d34f060f
Alan Huang <alan.huang.2@stonybrook.edu>
2022-07-20 20:20:54 -0400

    implemented middle for stylesheet access and created blog database using mongodb

--- app.js
+++ app.js
@@ -1,26 +1,88 @@
 const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const Blog = require('./models/blog.js');
 
-const app = express()
+const app = express();
 
+
+//connect to mongodb 
+const dburl = "Enter Data Base URL Here"
+mongoose.connect(dburl, {useNewUrlParser: true, useUnifiedTopology: true})
+//listen to request only when connected
+.then((result) => app.listen(3000))
+.catch((err) => console.log(err));
+
+console.log(mongoose.connection.readyState);
+
+//view engine
 app.set('view engine', 'ejs');
 
-app.listen(3000);
+//middleware 
+app.use(morgan('dev'));
+app.use(express.static('public'));
+
+// mongoose test routes
+// app.get('/add-blog', (req, res) =>{
+//     const blog = new Blog({
+//         title: "test Blog 2",
+//         snippet: "about new blog",
+//         body: "more about new blog"
+//     });
+
+//     blog.save().then((results) =>{
+//         res.send(results)
+//     }).catch((err) =>{
+//         console.log(err)
+//     });
+// });
+
+// app.get('/all-blogs', (req, res) =>{
+//     Blog.find().then((result) => {
+//         res.send(result);
+//     }).catch((err) =>{
+//         console.log(err);
+//     });
+// });
+
+// app.get('/single-blog', (req, res) => {
+//     Blog.findById('62d83c5e1a42d029ebdc2aee').then((result) =>{
+//         res.send(result);
+//     }).catch((err) =>{
+//         console.log(err);
+//     });
+// });
+
+
+
 
 app.get('/', (req, res) =>{
-    const blogs = [
-        {title: 'lorem', snippet: 'lorem ipsum dolor sit amet'},
-        {title: 'lorem2', snippet: 'lorem ipsum dolor sit amet'},
-        {title: 'lorem3', snippet: 'lorem ipsum dolor sit amet'}
-    ];
-    res.render('index', {
-        title: 'Home',
-        blogs
-    });
+    // const blogs = [
+    //     {title: 'lorem', snippet: 'lorem ipsum dolor sit amet'},
+    //     {title: 'lorem2', snippet: 'lorem ipsum dolor sit amet'},
+    //     {title: 'lorem3', snippet: 'lorem ipsum dolor sit amet'}
+    // ];
+    // res.render('index', {
+    //     title: 'Home',
+    //     blogs
+    // });
+    res.redirect('/blogs')
 });
 
 app.get('/about', (req, res) =>{
     res.render('about', {
         title: 'About'
+    });
+});
+
+app.get('/blogs', (req, res) =>{
+    Blog.find().sort({createdAt: -1}).then((result) =>{
+        res.render('index', {
+            title: 'All Blogs',
+            blogs: result
+        })
+    }).catch((err) =>{
+        console.log(err);
     });
 });
 
